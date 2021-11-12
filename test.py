6765a6694d84c9a07ee8afe7fe18c4193fbd7d33
andreaschh <andreasch771@gmai.com>
2021-11-12 21:08:49 +0200

    KJJK,

--- test.py
+++ test.py
@@ -1,20 +1,24 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+from PIL import Image
+image = Image.open('sunrise.jpg')
 
+st.image(image, width=150)
 st.title("Show model Data")
 
-if st.button('Say hello'):
-    st.write('test')
-else:
-    st.write('Goodbye')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+if st.button('Submit'):
+    color = st.select_slider(
+        'Select Rating Star',
+        options=['0', '1', '2', '3', '4', '5'])
+    st.write('Rating Star:', color)
+else:
+    st.write('')
 
 # initialize list of lists
-data = [['Show Data', (11199, 11328, 11287, 32345, 12342, 1232, 13456, 123244, 13456)],
+data = [['Select Option:', ''],
+        ['Show Data', (11199, 11328, 11287, 32345, 12342, 1232, 13456, 123244, 13456)],
         ['Clean Data', ('a', 'b', 'c')],
         ['Train Model', ("loc2", "loc1", "loc3", "loc1", "loc2", "loc2", "loc3", "loc2", "loc1")],
         ['Show Graphs', ("loc2", "loc1", "loc3", "loc1", "loc2", "loc2", "loc3", "loc2", "loc1")]]
