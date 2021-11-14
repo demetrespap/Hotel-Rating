a1a8c0af0284cd176dd53581ed6eecb0b3030833
demetrespap <dimitrispap1998@hotmail.com>
2021-11-14 11:35:57 +0200

    Update 5

--- apps/choose_model.py
+++ apps/choose_model.py
@@ -1,10 +1,20 @@
-import numpy as np
+import self as self
 import streamlit as st
+import spacy
 import pandas as pd
-from sklearn import datasets
+import numpy as np
+import seaborn as sns
+import math
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
 from sklearn.metrics import classification_report
+from apps import data
+import preprocess_kgptalkie as ps
+import re
+import string
+from sklearn.metrics import plot_confusion_matrix
+import imblearn
 
 
 def app():
@@ -26,3 +36,69 @@ def app():
     np.random.randn(20, 3),
     columns = ['a', 'b', 'c'])
     st.line_chart(chart_data)
+
+
+
+    # DEPENDED from choosed model
+    #*******************************
+
+    # get balaned data
+    code2 = '''def get_average_data(df):
+      max_num = (df.groupby(["Rating"]).count().sum()[0] / len(df["Rating"].unique()))
+      max_num = math.trunc(max_num)
+      new_df = pd.DataFrame(data = None, columns=['Review','Rating'])
+      while(len(new_df.query('Rating == 1'))<=max_num or len(new_df.query('Rating == 2')) <= max_num or len(new_df.query('Rating == 3')) <= max_num
+      or len(new_df.query('Rating == 4')) <= max_num or len(new_df.query('Rating == 5')) <= max_num):
+        for index, row in df.iterrows():
+          if (row['Rating'] == 5 and (len(new_df.query('Rating == 5')) <= max_num)):
+            df2 = pd.DataFrame({'Review': [row['Review']], 'Rating' : [row['Rating']]})
+            new_df = pd.concat([new_df, df2], ignore_index = True, axis = 0)
+          if (row['Rating'] == 4 and (len(new_df.query('Rating == 4')) <= max_num)):
+            df2 = pd.DataFrame({'Review': [row['Review']], 'Rating' : [row['Rating']]})
+            new_df = pd.concat([new_df, df2], ignore_index = True, axis = 0)
+          if (row['Rating'] == 3 and (len(new_df.query('Rating == 3')) <= max_num)):
+            df2 = pd.DataFrame({'Review': [row['Review']], 'Rating' : [row['Rating']]})
+            new_df = pd.concat([new_df, df2], ignore_index = True, axis = 0)
+          if (row['Rating'] == 2 and (len(new_df.query('Rating == 2')) <= max_num)):
+            df2 = pd.DataFrame({'Review': [row['Review']], 'Rating' : [row['Rating']]})
+            new_df = pd.concat([new_df, df2], ignore_index = True, axis = 0)
+          if (row['Rating'] == 1 and (len(new_df.query('Rating == 1')) <= max_num)):
+            df2 = pd.DataFrame({'Review': [row['Review']], 'Rating' : [row['Rating']]})
+            new_df = pd.concat([new_df, df2], ignore_index = True, axis = 0)
+        print(new_df['Rating'].value_counts())
+      return new_df'''
+    st.code(code2, 'python')
+
+    if st.button('Run', key=2):
+        df = data.reads_csv()
+        print(df)
+        new_df = get_average_data(df)
+        print(new_df['Rating'].value_counts())
+
+
+def get_average_data(df):
+    max_num = (df.groupby(["Rating"]).count().sum()[0] / len(df["Rating"].unique()))
+    max_num = math.trunc(max_num)
+    new_df = pd.DataFrame(data=None, columns=['Review', 'Rating'])
+    while (len(new_df.query('Rating == 1')) <= max_num or len(new_df.query('Rating == 2')) <= max_num or len(
+            new_df.query('Rating == 3')) <= max_num
+           or len(new_df.query('Rating == 4')) <= max_num or len(new_df.query('Rating == 5')) <= max_num):
+        for index, row in df.iterrows():
+            if (row['Rating'] == 5 and (len(new_df.query('Rating == 5')) <= max_num)):
+                df2 = pd.DataFrame({'Review': [row['Review']], 'Rating': [row['Rating']]})
+                new_df = pd.concat([new_df, df2], ignore_index=True, axis=0)
+            if (row['Rating'] == 4 and (len(new_df.query('Rating == 4')) <= max_num)):
+                df2 = pd.DataFrame({'Review': [row['Review']], 'Rating': [row['Rating']]})
+                new_df = pd.concat([new_df, df2], ignore_index=True, axis=0)
+            if (row['Rating'] == 3 and (len(new_df.query('Rating == 3')) <= max_num)):
+                df2 = pd.DataFrame({'Review': [row['Review']], 'Rating': [row['Rating']]})
+                new_df = pd.concat([new_df, df2], ignore_index=True, axis=0)
+            if (row['Rating'] == 2 and (len(new_df.query('Rating == 2')) <= max_num)):
+                df2 = pd.DataFrame({'Review': [row['Review']], 'Rating': [row['Rating']]})
+                new_df = pd.concat([new_df, df2], ignore_index=True, axis=0)
+            if (row['Rating'] == 1 and (len(new_df.query('Rating == 1')) <= max_num)):
+                df2 = pd.DataFrame({'Review': [row['Review']], 'Rating': [row['Rating']]})
+                new_df = pd.concat([new_df, df2], ignore_index=True, axis=0)
+        print(new_df['Rating'].value_counts())
+    return new_df
+    
--- apps/data.py
+++ apps/data.py
@@ -4,30 +4,18 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn import datasets
-
-
-
-
+import config
 def app():
 
     st.title('Data')
     st.write("This is the `Data` page of the multi-page app.")
     st.write("The following is the DataFrame of the `iris` dataset.")
     st.subheader("Dataset")
-
-    file=reads_csv()
-    print(file)
+    data_file = st.file_uploader("Upload CSV", type=['csv'])
+    if data_file is not None:
+        df = pd.read_csv(data_file)
+        file = pd.DataFrame(data=df)
+        config.file = file
     if st.button("Process"):
         if file is not None:
-
-            file_details = {"Filename": file.name, "FileType": file.type, "FileSize": file.size}
-            st.write(file_details)
-
             st.dataframe(file)
-
-@st.cache(allow_output_mutation=True,suppress_st_warning=True)
-def reads_csv():
-    data_file = st.file_uploader("Upload CSV", type=['csv'])
-    df = pd.read_csv(data_file)
-    return df
-
