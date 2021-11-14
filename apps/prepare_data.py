9cd4596f8e4aa82a64fbfe1f0476cf42318bf519
andreaschh <andreasch771@gmai.com>
2021-11-14 12:32:08 +0200

    Prepare Data fixed

--- apps/prepare_data.py
+++ apps/prepare_data.py
@@ -35,9 +35,14 @@ def get_clean(x):
   return x
 
 def app():
+  str.header('Prepare Data')
+  str.write("In this Page we clean the Data.We will capture the data from the csv file, from the previous page.")
   code = '''
   def get_clean(x):
-  x = str(x).lower().replace('\\', '').replace('_', ' ')
+  x = str(x)
+  x.lower(x)
+  x.replace('\\', '')
+  x.replace('_', ' ')  
   x = ps.cont_exp(x)
   x = ps.remove_emails(x)
   x = ps.remove_urls(x)
@@ -54,38 +59,18 @@ def app():
     new_df = get_average_data(df)
     print(new_df['Rating'].value_counts())
 
-  code2='''def get_average_data(df):
-  max_num = (df.groupby(["Rating"]).count().sum()[0] / len(df["Rating"].unique()))
-  max_num = math.trunc(max_num)
-  new_df = pd.DataFrame(data = None, columns=['Review','Rating'])
-  while(len(new_df.query('Rating == 1'))<=max_num or len(new_df.query('Rating == 2')) <= max_num or len(new_df.query('Rating == 3')) <= max_num
-  or len(new_df.query('Rating == 4')) <= max_num or len(new_df.query('Rating == 5')) <= max_num):
-    for index, row in df.iterrows():
-      if (row['Rating'] == 5 and (len(new_df.query('Rating == 5')) <= max_num)):
-        df2 = pd.DataFrame({'Review': [row['Review']], 'Rating' : [row['Rating']]})
-        new_df = pd.concat([new_df, df2], ignore_index = True, axis = 0)
-      if (row['Rating'] == 4 and (len(new_df.query('Rating == 4')) <= max_num)):
-        df2 = pd.DataFrame({'Review': [row['Review']], 'Rating' : [row['Rating']]})
-        new_df = pd.concat([new_df, df2], ignore_index = True, axis = 0)
-      if (row['Rating'] == 3 and (len(new_df.query('Rating == 3')) <= max_num)):
-        df2 = pd.DataFrame({'Review': [row['Review']], 'Rating' : [row['Rating']]})
-        new_df = pd.concat([new_df, df2], ignore_index = True, axis = 0)
-      if (row['Rating'] == 2 and (len(new_df.query('Rating == 2')) <= max_num)):
-        df2 = pd.DataFrame({'Review': [row['Review']], 'Rating' : [row['Rating']]})
-        new_df = pd.concat([new_df, df2], ignore_index = True, axis = 0)
-      if (row['Rating'] == 1 and (len(new_df.query('Rating == 1')) <= max_num)):
-        df2 = pd.DataFrame({'Review': [row['Review']], 'Rating' : [row['Rating']]})
-        new_df = pd.concat([new_df, df2], ignore_index = True, axis = 0)
-    print(new_df['Rating'].value_counts())
-  return new_df'''
-  str.code(code2,'python')
+  str.write("•The lower () method takes no arguments and returns the lowercased strings from the given string by converting each uppercase character to lowercase. "
+            "If there are no uppercase characters in the given string, it returns the original string.This code appears in the first line of the above code"
+            ":x = str(x).lower().replace('\\', '').replace('_', ' ').")
+  str.write("•Compound statements span multiple lines, although in simple incarnations a whole compound statement may be contained in one line. This code appears in the second line:x =ps.cont_exp(x).")
+  str.write("•Remove emails expression will delete if there is emails in the code. This code appears in the third line: x = ps.remove_emails(x).")
+  str.write("•Remove URLs expression will delete if there is URLs in the code. This code appears in the third line: x = ps.remove_urls(x).")
+  str.write("•Remove Html Tags expression will delete if there is Html Tags in the code. This code appears in the third line: x = ps.remove_html_tags(x).")
+  str.write("•Remove Strings expression will delete if there is Strings in the object added to filename. This code appears in the third line:   x = ps.remove_rt(x).")
+  str.write("•Remove Accented Characters expression will delete Unidecode and convert it to Ascii Code. This code appears in the third line:   x = ps.remove_accented_chars(x).")
+  str.write("•Remove Special Characters expression will delete all special characters in the code. This code appears in the third line:     x = ps.remove_special_chars(x).")
+  str.write("•This Regular Expression x = re.sub('(.)\\1{2,}', '\\1', x) is used to normalize the data.")
 
-  if str.button('Run', key=2):
-    x="hello"
-    x=get_clean(x)
-    str.write(x)
-    print(new_df['Review'])
-    new_df['Review'] = new_df['Review'].apply(lambda x: get_clean(x))
 
 
 def get_average_data(df):
