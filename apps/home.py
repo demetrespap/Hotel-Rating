fa0b7a7fdf598ca200eb03b89d0106c4f9e4b4fd
andreaschh <andreasch771@gmai.com>
2021-11-14 13:12:19 +0200

    yyyy

--- apps/home.py
+++ apps/home.py
@@ -6,8 +6,7 @@ import base64
 
 
 def app():
-    st.markdown(
-        "<h1 style='text-align: center;'>Hotel Rating Application</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>Hotel Rating Application</h1>", unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center; font-size:15px;'>This system was created for Hotel Rating Application. It has been created by three Postgraduate Students in the CEI-523 Course, Data Science. These students are Michalis Aristotelous, Dimitris Papadopoulos and Andreas Christodoulou. Supervising professor of this course is Mr. Andreas Christoforou.This model will accept as input a file and will have the ability to calculate with great accuracy the reviews depending on the text chosen by the user and will display the corresponding number from 1 to 5.</h2>", unsafe_allow_html=True)
 
     st.markdown("<h1 style='text-align: center;'>Contents</h2>", unsafe_allow_html=True)
